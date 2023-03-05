a25616a00ebd5eafb822a9cf7a54f8f2b9c13e04
Gilbert Hyman Goes <gilberthg@fairtech.com.sg>
2023-03-06 01:18:43 +0800

    implement resilience4j

--- inventory-service/src/main/java/sg/com/fairtech/inventory/service/service/InventoryService.java
+++ inventory-service/src/main/java/sg/com/fairtech/inventory/service/service/InventoryService.java
@@ -1,6 +1,8 @@
 package sg.com.fairtech.inventory.service.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import sg.com.fairtech.inventory.service.dto.InventoryResponse;
@@ -10,12 +12,19 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCode) {
+        // Try to make behavior if the server is going slow down
+//        log.info("Wait started");
+//        Thread.sleep(10000);
+//        log.info("Wait ended");
+
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventory ->
                     InventoryResponse.builder()

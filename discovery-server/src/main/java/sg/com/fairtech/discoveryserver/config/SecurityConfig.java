96cca6c77f2d42ffd478ee4e952916aaffbd466a
Gilbert Hyman Goes <gilberthg@fairtech.com.sg>
2023-03-05 22:49:54 +0800

    implement the spring security & key cloak on the api

---
+++ discovery-server/src/main/java/sg/com/fairtech/discoveryserver/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package sg.com.fairtech.discoveryserver.config;
+
+import org.glassfish.jersey.servlet.internal.spi.NoOpServletContainerProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cglib.proxy.NoOp;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.function.Function;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${eureka.username}")
+    private String username;
+
+    @Value("${eureka.password}")
+    private String password;
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        System.out.println(username);
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username(username)
+                .password(password)
+                .authorities("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+
+}
